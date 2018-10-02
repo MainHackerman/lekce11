59bcd0b892fd84a693bb636fe803e55bb4b7d5fb
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-10-02 19:27:57 +0100

    Pridal jsem odchytavani konkretnich chyb

--- chyby.py
+++ chyby.py
@@ -1,5 +1,13 @@
 #cislo = int('abc')
 print('ahoj')
 if 1 < 2:
-    print(1/'1')
+    #print(1/'1')
     print(3)
+vstup = input('Zadej cislo: ')
+try:
+    cislo = int(vstup)
+    vysledek = 100 / cislo
+except ValueError:
+    print('Zadala jsi blbost.')
+except ZeroDivisionError:
+    print('Nemuzes zadat 0.')
