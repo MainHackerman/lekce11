c6c6df69c3664093eabacba8b881bd05e7512c2a
Ondrej Tinka <ondrej.tinka@gmail.com>
2018-10-02 19:53:03 +0100

    Pridal jsem while true smycku

--- chyby.py
+++ chyby.py
@@ -3,11 +3,17 @@ print('ahoj')
 if 1 < 2:
     #print(1/'1')
     print(3)
-vstup = input('Zadej cislo: ')
-try:
-    cislo = int(vstup)
-    vysledek = 100 / cislo
-except (ValueError, ZeroDivisionError):
-    print('Zadala jsi blbost.')
-finally:
-    print('Dekujeme ze pouzavet nasi aplikaci')
+
+while True:
+    vstup = input('Zadej cislo: ')
+    try:
+        cislo = int(vstup)
+        vysledek = 100 / cislo
+    except (ValueError, ZeroDivisionError):
+        print('Zadala jsi blbost.')
+    else:
+        print('vysledek je: ', vysledek)
+        break
+
+nasobek = cislo * 37
+print(nasobek)
